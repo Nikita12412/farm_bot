97475b8e41de6ff6d1c5f48e1f4adb6b7526a4d1
Пользователь <dsgsd@mail.com>
2026-02-13 17:57:08 +0300

    first commit added comand start

---
+++ main.py
@@ -0,0 +1,15 @@
+from aiogram import Dispatcher,Bot,types
+from aiogram import F
+from aiogram.filters import Command,CommandObject
+import asyncio
+
+apitoken = '8557827638:AAG21pzqTqu6FCOiAYBRqi9Q8eDSbCraV1c'
+bot = Bot(apitoken)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def start(message:types.Message):
+    await message.answer("Привет")
+
+
+asyncio.run(dp.start_polling(bot))
