b29d21d01315611dcd6893218627d1f1c77ed683
Пользователь <dsgsd@mail.com>
2026-02-19 16:48:39 +0300

    изменение команды старт

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ dp = Dispatcher()
 
 @dp.message(Command('start'))
 async def start(message:types.Message):
-    await message.answer("Привет")
+    await message.answer("Приветствую вас, хозяин фермы! 🏡\nВы попали в симулятор фермерской жизни. У вас есть участок земли, стартовый капитал и море возможностей.\nЧто доступно:\n🔸 посадка культур и сбор урожая;\n🔸 улучшение фермы;\n🔸 торговля на рынке.\nИспользуйте меню ниже, чтобы начать своё приключение!")
 
 asyncio.run(dp.start_polling(bot))
